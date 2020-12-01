4a8a9ec559edfcbb7a4e2741c16a693b95aa2b6a
Andrey Fomenkov <droideka@mail.ru>
2020-12-01 21:22:27 +0300

    Updating report.

--- src/test/kotlin/ru/fomenkov/rxdisposablewatcher/HtmlReportBuilderTest.kt
+++ src/test/kotlin/ru/fomenkov/rxdisposablewatcher/HtmlReportBuilderTest.kt
@@ -74,8 +74,8 @@ class HtmlReportBuilderTest : BaseTest() {
         const val TEMPLATE_REPORT = "$TEMPLATE_DIR/report.html"
         const val TEMPLATE_LINE_CHECKSUM = -376419563
         const val TEMPLATE_BLOCK_CHECKSUM = 2088613418
-        const val TEMPLATE_REPORT_CHECKSUM = -795649292
-        const val TEST_OUTPUT_HTML_REPORT_CHECKSUM = -74823822
+        const val TEMPLATE_REPORT_CHECKSUM = -425549768
+        const val TEST_OUTPUT_HTML_REPORT_CHECKSUM = 1121436812
         const val PLACEHOLDER_VALUE_LINE = "#value-line"
         const val PLACEHOLDER_VALUE_COLOR = "#value-color"
         const val PLACEHOLDER_VALUE_DETAILS = "#value-details"
