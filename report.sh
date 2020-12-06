9c334bd59c8943d4cc52a98061fd5f22e18ffb55
Andrey Fomenkov <droideka@mail.ru>
2020-12-06 21:39:34 +0300

    Shell script for Android device.

---
+++ report.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script has the next steps:
+# - send broadcast event to Android device;
+# - wait a little bit;
+# - pull HTML report & display in a browser.
+# REPLACE! placeholders <PATH_ON_SDCARD> and <PATH_ON_DESKTOP>
+
+BROADCAST_ACTION="com.testapp.rxreport"
+SLEEP_BEFORE_PULL=2 # Delay in seconds
+# Source location in Android device SD card
+REPORT_DEVICE_LOCATION=<PATH_ON_SDCARD>/report.html
+# Target location on a desktop
+REPORT_DESKTOP_LOCATION=<PATH_ON_DESKTOP>/report.html
+
+adb shell am broadcast -a $BROADCAST_ACTION
+sleep $SLEEP_BEFORE_PULL
+adb pull $REPORT_DEVICE_LOCATION $REPORT_DESKTOP_LOCATION
+adb shell rm $REPORT_DEVICE_LOCATION
+open $REPORT_DESKTOP_LOCATION
